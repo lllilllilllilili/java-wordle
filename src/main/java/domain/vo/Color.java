bd4a41a27d34a308a7813a6b257e211eb2b48ad4
a1101710 <ingyuhwang@sk.com>
2022-06-06 22:00:07 +0900

    feat(wordle) : color enum 추가

---
+++ src/main/java/domain/vo/Color.java
@@ -0,0 +1,15 @@
+package domain.vo;
+
+public enum Color {
+    GREEN("\uD83D\uDFE9"),
+    YELLOW("\uD83D\uDFE8"),
+    GRAY("\u2B1C");
+
+    private final String box;
+
+    Color(String box) { this.box = box; }
+
+    public String getBox() {
+        return box;
+    }
+}
