8fb8a71a1c874f8adbd2c5e98b7c484e0fde0edd
a1101710 <ingyuhwang@sk.com>
2022-06-07 09:16:39 +0900

    feat(wordle) : UserConsole 추가

--- src/main/java/view/UserConsole.java
+++ src/main/java/view/UserConsole.java
@@ -6,11 +6,16 @@ import java.util.Scanner;
 
 public class UserConsole {
 
-    private static final Scanner SCANNER = new Scanner(System.in);
+    private final Scanner SCANNER;
     private static final String FIRST_MESSAGE = "문자열을 입력해주세요.";
 
-    public static UserInput inputString() {
-        System.out.println(FIRST_MESSAGE);
+    //생성할 때 SCANNER가 나오는것을 확인할 수 있음
+    public UserConsole() {
+        SCANNER = new Scanner(System.in);
+    }
+
+    //만들어 두고 나중에 실행한다.
+    public UserInput inputUserConsole() {
         return UserInput.validateUserInput(SCANNER.nextLine());
     }
 
