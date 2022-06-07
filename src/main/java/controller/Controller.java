b54defc4a32a4f9ecd7823438d1396ce0e481f39
a1101710 <ingyuhwang@sk.com>
2022-06-07 09:15:13 +0900

    feat(wordle) : Controller 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,13 +1,22 @@
 package controller;
 
+import domain.JudgementImpl;
 import domain.state.AnswerGenerator;
+import domain.state.Judgement;
 import domain.vo.UserInput;
 import utils.AnswerGeneratorImpl;
 import view.UserConsole;
 
 public class Controller {
     public static void run() {
-        UserInput userInput = UserConsole.inputString();
-        AnswerGenerator answerGenerator = new AnswerGeneratorImpl();
+
+
     }
+
+    public static void init() {
+         UserConsole userConsole = new UserConsole();
+         AnswerGenerator answerGenerator = new AnswerGeneratorImpl();
+         Judgement judgement = new JudgementImpl();
+    }
+
 }
